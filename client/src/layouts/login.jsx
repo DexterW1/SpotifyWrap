8963421db302fa195b1c08e7fae8683952c4ee22
Yiu Ming Wong <56940477+DexterW1@users.noreply.github.com>
2023-11-09 19:02:57 -0800

    added changeable loginurl

--- client/src/layouts/login.jsx
+++ client/src/layouts/login.jsx
@@ -1,10 +1,11 @@
 import "./login.css";
+const url = import.meta.env.VITE_LOGIN || "http://localhost:5000/login";
 export default function Login() {
   return (
     <>
       <div className="login-container">
         <h1>Spotify Wrap</h1>
-        <a href="http://localhost:5000/login">LOGIN TO SPOTIFY</a>
+        <a href={url}>LOGIN TO SPOTIFY</a>
       </div>
     </>
   );
